968d2cff65f2c798dabc6f6c470c1d54981e02c4
Taisiia Loginova <lebei@mail.ry>
2024-07-11 15:03:38 +0300

    first commit

---
+++ Homework/script.js
@@ -0,0 +1,8 @@
+// Task 1
+let a = 22;
+let FarTemp = (9/5) * a + 32;
+console.log(FarTemp);
+// Task 2
+let fname = 'Таисия'
+let admin = fname;
+console.log(admin);
---
+++ Seminar/script.js
@@ -0,0 +1,43 @@
+let num = 123;
+console.log("num =" + num);
+// comment 1
+let a = 5;
+a = 7;
+console.log("a = " + a);
+// comment 2
+let b = 1 + 2 * 3;
+console.log("b = " + b);
+// excisize 1
+let c = 10;
+let d = 2;
+let sum = c + d;
+console.log(sum);
+let diff = c - d;
+console.log(diff);
+let comp = c*d;
+console.log(comp);
+let div = c/d;
+console.log(div);
+let result = sum + diff + comp + div;
+console.log(result);
+
+// excizise 3 
+const n = 1.5;
+const l = 0.75;
+
+let res = n + l;
+console.log(res);
+let cet = -5;
+console.log(cet*-1)
+
+// ex 4
+const fname = 'Таисия'
+const lname = 'Логинова'
+console.log(fname,lname);
+const j = 'java'
+const s = 'script'
+console.log(j + s);
+const h = 'hello'
+const w = 'world'
+console.log
+
